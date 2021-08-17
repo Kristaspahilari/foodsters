1ca36c97f35087a8c472d77693844cdb9959412e
Krista <krista.spahillari@gmail.com>
2021-08-17 12:06:49 +0200

    more info

--- app.py
+++ app.py
@@ -4,5 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('base.html')
+    f = open('grains.txt')
+    grains= f.read().splitlines()
+    f = open('vegetables.txt')
+    vegetables= f.read().splitlines()
+    f = open('fruits.txt')
+    fruits= f.read().splitlines()
+    f = open('dairy.txt')
+    dairy= f.read().splitlines()
+    f = open('meat.txt')
+    meat= f.read().splitlines()
+    
+    
+    
+    return render_template('base.html', grains_list=grains, vegetables_list=vegetables, fruits_list=fruits, dairy_list=dairy, meat_list=meat) 
     
